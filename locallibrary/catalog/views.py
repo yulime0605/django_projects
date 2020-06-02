9fa657f8b0cf8ec6328532dbaae37173f6999023
Yu Lim Kim <yulime0605@naver.com>
2020-06-02 16:22:21 +0900

    part7

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -15,13 +15,19 @@ def index(request):
     # The 'all()' is implied by default.    
     num_authors = Author.objects.count()
     
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_visits': num_visits,
     }
-    # Render the HTML template index.html with the data in the context variable
+    
+    # Render the HTML template index.html with the data in the context variable.
     return render(request, 'index.html', context=context)
 
 class BookListView(generic.ListView):
